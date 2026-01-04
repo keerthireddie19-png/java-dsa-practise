f45a6134507c040b0f227550851755c12a57336c
soma keerthi <keerthireddie19@gmail.com>
2026-01-04 14:33:29 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,17 @@
+class EvenCheck {
+    public static void main(String[] args)
+{
+    int a=10;
+    int b=15;
+    if(a/2==0){
+        System.out.println(a + " is Even");
+    } else {
+        System.out.println(a + " is odd");
+    }
+    if(b/2==0){
+      System.out.println(b + " is Even");
+    } else{
+        System.out.println(b + " is odd"); 
+    }
+}
+}
